3916f05de7fa29783b3a4edbdfe32847c1d0a05b
LinkFly <linkfly1@newmail.ru>
2013-11-02 00:05:49 +0300

    init commit

---
+++ remloc-call-client.lisp
@@ -0,0 +1,34 @@
+;(ql:quickload :cl-store)
+;(ql:quickload :usocket)
+
+(defpackage :remote-call 
+  (:use :cl :usocket :cl-store))
+
+(in-package :remote-call)
+
+(defparameter *remote-call-stream* nil)
+
+(defun get-remote-call-stream (host port)
+  (socket-connect host port))
+
+(defun connect-to-remote-call-server (host port &optional (remote-call-stream-sym '*remote-call-stream*))
+ (set remote-call-stream-sym (get-remote-call-stream host port)))
+
+;(connect-to-remote-call-server "127.0.0.1" 2002)
+
+(defmacro def-call (name remote-function &key (remote-call-stream-sym '*remote-call-stream*)  &aux name-str)
+  ;(setf name "FUN1")
+  `(defun ,name (&rest args)
+     (let ((stream (socket-stream (symbol-value ',remote-call-stream-sym))))
+       (store (list ,remote-function args) stream)
+       (force-output stream)
+       (restore stream))))
+
+(def-call fn1 "FUN1")
+(def-call fn2 "FUN2")
+(def-call fn3 "FUN3")
+(def-call fn4 "FUN4")
+(fn1 2)
+(fn2 2)
+(fn3 2)
+(fn4 2)
---
+++ remloc-call-server.lisp
@@ -0,0 +1,52 @@
+;(ql:quickload :cl-store)
+;(ql:quickload :usocket)
+;(ql:quickload :bordeaux-threads)
+
+(defpackage :remote-call 
+  (:use :cl :usocket :cl-store :bordeaux-threads))
+
+(in-package :remote-call)
+
+(defparameter *reg-call* (make-hash-table :test 'equal))
+
+(defun as-string (obj)
+  (typecase obj
+    (symbol (symbol-name obj))
+    (string obj)))
+
+(defun reg-call (name)
+  (setf (gethash (as-string name) *reg-call*) name))
+
+(defun fun1 (&args) "Hi!!!")
+(defun fun2 (&args) "Hello world!!!")
+(defun fun3 (&args) "fun3!!!")
+(defun fun4 (&args) "fun444!!!")
+;(reg-call 'fun1)
+;(reg-call 'fun2)
+;(reg-call 'fun3)
+;(reg-call 'fun4)
+
+(defun handler (fn-name args &aux fn-sym)
+  (setf fn-sym (gethash fn-name *reg-call*))
+  (when fn-sym
+    (funcall fn-sym args)))
+
+(defun start-remote-call-server (host port &aux (seq (make-array 5 :element-type '(unsigned-byte 8))))
+  (make-thread 
+   (lambda ()
+     (loop :with socket = (usocket:socket-listen host port)
+           :with stream = (socket-stream (socket-accept socket))
+           :while t
+           :do (destructuring-bind (remote-function args)
+                   (restore stream)
+                 (store (handler remote-function args) stream)
+                 (force-output stream))))
+   :name (format nil "Remote call server (~A ~A)" host port)))
+
+ 
+
+
+  
+
+
+             
